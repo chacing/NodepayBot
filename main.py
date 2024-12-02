f11be78c6e66973a8cc3a6026dae0fb33e791ef6
Enukio <38923168+Enukio@users.noreply.github.com>
2024-12-03 02:08:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ DOMAIN_API_ENDPOINTS = {
         "https://api.nodepay.ai/api/auth/session"
     ],
     "PING": [
-        "http://18.142.29.174/api/network/ping"
+        "https://nw2.nodepay.org/api/network/ping"
     ]
 }
 
