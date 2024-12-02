f5d589c82913b7e931b7b9989e701b6117f49500
Enukio <38923168+Enukio@users.noreply.github.com>
2024-12-03 02:17:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,11 +59,10 @@ RETRIES_LIMIT = 60
 
 # API Endpoints
 DOMAIN_API_ENDPOINTS = {
-    "SESSION": [
-        "https://api.nodepay.ai/api/auth/session"
-    ],
-    "PING": [
-        "https://nw2.nodepay.org/api/network/ping"
+    "SESSION": "https://api.nodepay.ai/api/auth/session",
+    "PING": [ 
+        "http://18.142.29.174/api/network/ping",
+        "https://nw.nodepay.org/api/network/ping" 
     ]
 }
 
